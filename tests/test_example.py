0f15d9ce4d1da3ec888c801fd3cc0250ad6238f2
Daniil Balabanov <spacewalrus@bk.ru>
2023-01-25 19:47:02 +0300

    add new tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,5 @@
 from hexlet_pytest.example import reverse
+import pytest
 
 
 def test_reverse():
@@ -8,3 +9,26 @@ def test_reverse():
 def test_reverse_for_empty_string():
     assert reverse('') == ''
 
+
+def test_stack():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empthy_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
